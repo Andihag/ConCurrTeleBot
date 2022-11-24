c92c23ff7e565243546e6ad9fd08cfe61c4d8cf2
Andyhag <a-atyakshev56@yandex.ru>
2022-11-24 22:25:31 +0500

    add TeleBot

---
+++ config.py
@@ -0,0 +1,8 @@
+TOKEN = "5876892371:AAFpsf0ySwHrmbBmya0JqMrkTLJtqGoN0YA"
+CURRENCY_API = "7dfba99f329fd57790e6662a96b62d1cd769df513d7e3ae5bc27dbfd0d27e004"
+
+keys = {
+    "евро": "EUR",
+    "доллар": "USD",
+    "рубль": "RUB",
+}
